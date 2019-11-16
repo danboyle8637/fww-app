2e66b825fab445f2b306d4743dc119e442098291
Dan Boyle <danboyle8637@gmail.com>
2019-11-16 17:10:58 -0500

    Used picture tag to make my background images responsive. It works great!

--- src/hooks/useRenderBackgroundImage.js
+++ src/hooks/useRenderBackgroundImage.js
@@ -1,7 +1,5 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import styled from 'styled-components'
-
-import ScreenWidthContext from '../context/ScreenWidthContext'
 
 const useRenderBackgroundImage = (
   mobileBackground,
@@ -11,33 +9,50 @@ const useRenderBackgroundImage = (
   title,
   alt
 ) => {
-  const device = useContext(ScreenWidthContext)
-
-  switch (device) {
-    case 'mobile': {
-      return <BackgroundImage src={mobileBackground} title={title} alt={alt} />
-    }
-    case 'tablet': {
-      return <BackgroundImage src={tabletBackground} title={title} alt={alt} />
-    }
-    case 'ipadPro': {
-      return <BackgroundImage src={ipadProBackground} title={title} alt={alt} />
-    }
-    case 'laptop': {
-      return <BackgroundImage src={laptopBackground} title={title} alt={alt} />
-    }
-    case 'ultraWide': {
-      return <BackgroundImage src={laptopBackground} title={title} alt={alt} />
-    }
-    default: {
-      return <BackgroundImage src={laptopBackground} title={title} alt={alt} />
-    }
-  }
+  return (
+    <BackgroundImage>
+      <SourceBackgroundImage
+        media="(min-width: 1100px)"
+        srcSet={laptopBackground}
+      />
+      <SourceBackgroundImage
+        media="(min-width: 960px)"
+        srcSet={ipadProBackground}
+      />
+      <SourceBackgroundImage
+        media="(min-width: 600px)"
+        srcSet={tabletBackground}
+      />
+      <BaseBackgroundImage src={mobileBackground} alt={alt} title={title} />
+    </BackgroundImage>
+  )
 }
 
 export default useRenderBackgroundImage
 
-const BackgroundImage = styled.img`
+const BackgroundImage = styled.picture`
+  margin: 0;
+  padding: 0;
+  grid-column: 1 / -1;
+  grid-row: 1 / -1;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr;
+  width: 100%;
+`
+
+const SourceBackgroundImage = styled.source`
+  margin: 0;
+  padding: 0;
+  grid-column: 1 / -1;
+  grid-row: 1 / -1;
+  align-self: end;
+  width: 100%;
+`
+
+const BaseBackgroundImage = styled.img`
+  margin: 0;
+  padding: 0;
   grid-column: 1 / -1;
   grid-row: 1 / -1;
   align-self: end;
